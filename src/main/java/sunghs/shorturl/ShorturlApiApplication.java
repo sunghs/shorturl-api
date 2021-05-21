010355998081e7d44259e739113e1ad6350b37a6
sunghs <sunghs9071@naver.com>
2021-05-21 18:00:29 +0900

    Initial commit

---
+++ src/main/java/sunghs/shorturl/ShorturlApiApplication.java
@@ -0,0 +1,13 @@
+package sunghs.shorturl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShorturlApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShorturlApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sunghs/shorturl/ShorturlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package sunghs.shorturl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShorturlApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
