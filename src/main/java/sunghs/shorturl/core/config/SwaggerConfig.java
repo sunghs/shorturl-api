a9e998abe08540b6aaf3c8dfc8f1737951e32196
sunghs <sunghs9071@naver.com>
2021-05-21 18:26:59 +0900

    swagger apiInfo 추가

--- src/main/java/sunghs/shorturl/core/config/SwaggerConfig.java
+++ src/main/java/sunghs/shorturl/core/config/SwaggerConfig.java
@@ -2,8 +2,10 @@ package sunghs.shorturl.core.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -18,6 +20,19 @@ public class SwaggerConfig {
             .select()
             .apis(RequestHandlerSelectors.basePackage("sunghs.shorturl"))
             .paths(PathSelectors.any())
+            .build()
+            .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        String description = "단축 URL 서비스를 제공하는 API 입니다."
+            + System.lineSeparator()
+            + "자세한 사항은 README.md 에 첨부되어 있습니다.";
+
+        return new ApiInfoBuilder()
+            .title("shorturl-api")
+            .description(description)
+            .version("1.0.0")
             .build();
     }
 }
